0ca63861f72845e380db465bf3a6892ec811568d
Farid Aleskerov <kenigsbergrf@mail.ru>
2019-02-10 17:20:21 +0300

    Creating a cards page

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <product-cards></product-cards>
+</template>
+
+<script>
+import ProductCards from './components/ProductCards/ProductCards.vue'
+export default {
+  name: 'app',
+  components: {
+    ProductCards
+  }
+}
+</script>
+
+<style lang="less">
+@import './less/mixins.less';
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+body {
+  width: 100%;
+  background-image: linear-gradient(0deg,rgba(0,0,0,.5) 0,transparent 50%,rgba(0,0,0,.5)),url(./assets/main_bg.png);
+  min-height: 100vh;
+  .flex-display();
+  .justify-content(center);
+  .align-items(center);
+
+}
+</style>
+
---
+++ src/components/ProductCards/ProductCards.vue
@@ -0,0 +1,3 @@
+<template src="./product-cards.html"></template>
+<script src="./product-cards.js"></script>
+<style lang="less" src="./product-cards.less"></style>
